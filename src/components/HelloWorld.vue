5c4317cb2165b7aed1905995a2e060c7cfb40f94
ZhangJunyi <zhangjunyi@zenmen.com>
2022-06-06 10:21:45 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <DatePicker></DatePicker>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script lang="ts" setup>
+import DatePicker from 'vue-datepicker-next';
+</script>
+
+<style scoped lang="scss">
+a {
+  color: #42b983;
+}
+</style>
